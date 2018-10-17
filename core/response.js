cad211e03e9270818058865d8a2922fb0a129578
Scott McGowan <scott.mcgowan@smartsheet.com>
2018-10-17 10:31:19 +0100

    #7 - Add Status.END to available status
    Adding comments to status values to better explain the functionality due to the similarity for STOP, END, and PAUSE

--- core/response.js
+++ core/response.js
@@ -9,15 +9,23 @@
 
 const _ = require('lodash');
 
-module.exports = (function() {
+module.exports = (function () {
 
   class Status {
-    static get SUCCESS    () { return 0; }
-    static get FAIL       () { return 1; }
-    static get RETRY      () { return 2; }
-    static get STOP       () { return 3; }
-    static get PAUSE      () { return 4; }
-    static get NEED_AUTH  () { return 5; }
+    // The function execution was a success.
+    static get SUCCESS() { return 0; }
+    // The function execution has failed.
+    static get FAIL() { return 1; }
+    // The function execution should be retried.
+    static get RETRY() { return 2; }
+    // Stop the current conversation flow and any parent conversations.
+    static get STOP() { return 3; }
+    // Pause the current conversation flow, this will block the conversation until it is restarted.
+    static get PAUSE() { return 4; }
+    // Pause the current conversation flow and start the OAuth authentication flow. The conversation will be restarted when authentication is successful.
+    static get NEED_AUTH() { return 5; }
+    // End the current conversation flow, will allow parent conversations to continue.
+    static get END() { return 6; }
   }
 
 
